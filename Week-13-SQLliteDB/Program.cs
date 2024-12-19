0abd8a57c4f5182c738a53ac7ddc3857a7b312c4
Yippee.max <GabeN3well@gmail.com>
2024-12-19 04:05:05 +0200

    week13 SQlite DB

---
+++ Week-13-SQLliteDB/Program.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualBasic;
+using System.Data;
+using System.Data.SQLite;
+// loob DBga uhendus
+
+ReadData(CreateConnection());
+//InsertCustomer(CreateConnection());
+//RemoveCustomer(CreateConnection());
+static SQLiteConnection CreateConnection()
+{
+    SQLiteConnection connection = new SQLiteConnection("Data Source =mydb.db; Version = 3; New = True; Compress = True");
+    try
+    {
+        connection.Open();
+        Console.WriteLine("DB found!!! Yipee!");
+    }
+    catch
+    {
+        Console.WriteLine("DB not found, yeouch.");
+    }
+    return connection;
+}
+
+
+static void ReadData(SQLiteConnection myConnection)
+{
+    Console.Clear();
+    SQLiteDataReader reader;
+    SQLiteCommand command;
+
+    command = myConnection.CreateCommand();
+    command.CommandText = "SELECT rowid, * FROM customer";
+        //"SELECT customer.firstName, customer.lastName, status.statustype " +
+        //"FROM customerStatus " +
+        //"JOIN customer on customer.rowid = customerStatus.customerid " +
+        //"JOIN status on status.rowid = customerStatus.statusID " +
+        //"ORDER BY status.statustype";
+
+    reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        string readerRowId = reader["rowid"].ToString();
+        string readerStringFirstName = reader.GetString(1);
+        string readerStringLastName = reader.GetString(2);
+        string readerStringDoB = reader.GetString(3);
+
+        Console.WriteLine($"{readerRowId}.Full name: {readerStringFirstName} {readerStringLastName}; DoB: {readerStringDoB}");
+    }
+    myConnection.Close();
+}
+
+static void InsertCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+    string fName, lName, dob;
+
+    Console.WriteLine("Enter first name:");
+    fName = Console.ReadLine();
+    Console.WriteLine("Enter last name:");
+    lName = Console.ReadLine();
+    Console.WriteLine("Enter date of birth (mm-dd-yy):");
+    dob = Console.ReadLine();
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"INSERT INTO customer(firstName,lastName,dateOfBirth) " +
+    $"VALUES ('{fName}', '{lName}', '{dob}')";
+
+    int rowInserted = command.ExecuteNonQuery();
+    Console.WriteLine($"Row insrted: {rowInserted}");
+
+    
+    ReadData(myConnection);
+}
+
+static void RemoveCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+
+    string idToDelete;
+    Console.WriteLine("Enter an id to delete a customer");
+    idToDelete = Console.ReadLine();
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"DELETE FROM customer WHERE rowid = {idToDelete}";
+    int rowsDeleted = command.ExecuteNonQuery();
+    Console.WriteLine($"{rowsDeleted} was removed from the customer table");
+    ReadData(myConnection);
+}
